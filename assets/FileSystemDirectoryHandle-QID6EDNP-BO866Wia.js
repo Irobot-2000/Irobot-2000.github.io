4ce7089420f443d0257e6df68eaca5455ed77d70
Nilana <nilanasilva1@gmail.com>
2024-09-01 21:16:34 +0530

    Set actual signalling server

---
+++ assets/FileSystemDirectoryHandle-QID6EDNP-BO866Wia.js
@@ -0,0 +1,1 @@
+import{h as a,i}from"./index-DIP_7Pq0.js";import"https://cdnjs.cloudflare.com/ajax/libs/simple-peer/9.11.1/simplepeer.min.js";export{a as FileSystemDirectoryHandle,i as default};
---
+++ assets/FileSystemFileHandle-LKASIK6A-CpZHC2ho.js
@@ -0,0 +1,1 @@
+import{F as t,b as i}from"./index-DIP_7Pq0.js";import"https://cdnjs.cloudflare.com/ajax/libs/simple-peer/9.11.1/simplepeer.min.js";export{t as FileSystemFileHandle,i as default};
